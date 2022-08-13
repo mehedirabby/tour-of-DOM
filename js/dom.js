d2ab243eefe884b68884ff2cfbb0a139c3c25bf6
unknown <mehedi15-2995@diu.edu.bd>
2022-08-13 08:09:03 +0600

    another-commit

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
-console.log('hello from js');
-console.log(document.body)
+//console.log('hello from js');
+//console.log(document.body)
 const  allHeading = document.getElementsByTagName('h1');
 for(const h1 of allHeading){
-    console.log(h1.innerText);
+    //console.log(h1.innerText);
 }
